8c65c7fd737ca3fae61912dd584b911bddc186cc
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-05-26 15:48:22 +0530

    Remove No blog error7

--- src/Components/AllBlogs.js
+++ src/Components/AllBlogs.js
@@ -2,30 +2,26 @@ import { getBlogs } from "@/Functions/getBlogs";
 import React from "react";
 import BlogCard from "./BlogCard";
 
-export default async function AllBlogs() {
-  const { body } = await getBlogs();
-  if (!body) {
-    return (
-      <section className="mt-5 grid grid-cols-1  h-90 md:grid-cols-3 gap-5 justify-between overflow-y-scroll flex-wrap ">
-        Not found
-      </section>
-    );
-  }
+const AllBlogs = async () => {
+  const blogs = await getBlogs();
   return (
     <section className="mt-5 grid grid-cols-1  h-90 md:grid-cols-3 gap-5 justify-between overflow-y-scroll flex-wrap ">
-      {body.map((i, index) => {
-        return (
-          <BlogCard
-            key={index}
-            title={i.title}
-            category={i.category}
-            description={i.description.substring(0, 100) + "..."}
-            image={i.image}
-            id={i._id}
-            views={i.views}
-          />
-        );
-      })}
+      {blogs &&
+        blogs.body.map((i, index) => {
+          return (
+            <BlogCard
+              key={index}
+              title={i.title}
+              category={i.category}
+              description={i.description.substring(0, 100) + "..."}
+              image={i.image}
+              id={i._id}
+              views={i.views}
+            />
+          );
+        })}
     </section>
   );
-}
+};
+
+export default AllBlogs;
