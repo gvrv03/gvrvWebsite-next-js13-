d6c541f285c028554688635c152735e07402a3ab
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-06-23 18:58:08 +0530

    Create Product backend Add And Fetch Data 6

--- src/Components/Product/Allproducts.js
+++ src/Components/Product/Allproducts.js
@@ -5,7 +5,6 @@ import ProductCard from "./ProductCard";
 export default async function Allproducts() {
   const res = await fetch(getProductsURL);
   const data = await res.json();
-  console.log(data);
   if (data === undefined) {
     return (
       <div className="h-screen w-full grid place-items-center  bg-white ">
@@ -21,31 +20,32 @@ export default async function Allproducts() {
         </div>
       )}
       <section className="mt-5 grid grid-cols-1  h-90 md:grid-cols-4 gap-5 justify-between overflow-y-scroll flex-wrap ">
-        {data && data.map((product, index) => {
-          const {
-            title,
-            thumbnail,
-            pricing,
-            // productOrganization,
-            // addeBy,
-            _id,
-          } = product;
-          const { price, comAtPrice } = pricing;
+        {data &&
+          data.map((product, index) => {
+            const {
+              title,
+              thumbnail,
+              pricing,
+              // productOrganization,
+              // addeBy,
+              _id,
+            } = product;
+            const { price, comAtPrice } = pricing;
 
-          // const { category, type, vendor, collection, keywords } =
-          //   productOrganization;
+            // const { category, type, vendor, collection, keywords } =
+            //   productOrganization;
 
-          return (
-            <ProductCard
-              key={index}
-              id={_id}
-              title={title}
-              thumbnail={thumbnail}
-              price={price}
-              comAtPrice={comAtPrice}
-            />
-          );
-        })}
+            return (
+              <ProductCard
+                key={index}
+                id={_id}
+                title={title}
+                thumbnail={thumbnail}
+                price={price}
+                comAtPrice={comAtPrice}
+              />
+            );
+          })}
       </section>
     </>
   );
