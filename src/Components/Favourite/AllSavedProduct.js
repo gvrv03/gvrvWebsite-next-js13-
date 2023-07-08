d9df30a8f524e46191a0f223ccef3c81f0125173
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-07-08 14:34:42 +0530

    Add products to favorite done 6

--- src/Components/Favourite/AllSavedProduct.js
+++ src/Components/Favourite/AllSavedProduct.js
@@ -10,7 +10,6 @@ export default async function AllSavedProducts() {
   );
 
   const { products } = await res.json();
-  //   console.log(products);
   if (products && products === undefined) {
     return (
       <div className="h-screen w-full grid place-items-center  bg-white ">
@@ -21,28 +20,27 @@ export default async function AllSavedProducts() {
 
   return (
     <>
-      {products && products?.length === 0 && (
+      {products?.length === 0 && (
         <div className="w-full h-90 grid place-items-center bg-white mt-5">
           No Favourite Found
         </div>
       )}
 
       <section className=" grid grid-cols-2  md:grid-cols-4 gap-5  ">
-        {products &&
-          products?.map((product, index) => {
-            console.log(product);
-            return (
-              <ProductCard
-                key={index}
-                id={product?.productID?._id}
-                fullTitle={product?.productID?.title}
-                title={product?.productID?.title?.substring(0, 40) + "..."}
-                thumbnail={product?.productID?.thumbnail}
-                price={product?.productID?.pricing?.price}
-                comAtPrice={product?.productID?.pricing?.comAtPrice}
-              />
-            );
-          })}
+        {products?.map((product, index) => {
+          const { productID } = product ? product : {};
+          return (
+            <ProductCard
+              key={index}
+              id={productID?._id}
+              fullTitle={productID?.title}
+              title={productID?.title?.substring(0, 40) + "..."}
+              thumbnail={productID?.thumbnail}
+              price={productID?.pricing?.price}
+              comAtPrice={productID?.pricing?.comAtPrice}
+            />
+          );
+        })}
       </section>
     </>
   );
