6aeacde99476c888426a110808a77bd291a1ef68
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-07-14 10:29:06 +0530

    Add Meta Tags 3

--- src/app/Products/layout.js
+++ src/app/Products/layout.js
@@ -1,5 +1,69 @@
-import ProductCard from "@/Components/Product/ProductCard";
 import React from "react";
+
+
+
+export const metadata = {
+  title: "Products || Gaurav Narnaware",
+  description: "Premium Digital products that helps to complete your need ",
+  keywords: [
+    "Products",
+    "DigitalProducts",
+    "WebTemplates",
+    "WebSites",
+    "Ebooks",
+    "Templates",
+    "Presets",
+  ],
+  images: ["/gaurav.svg"],
+  url: "/Products",
+  twitter: {
+    card: "summary_large_image",
+    title: "Products || Gaurav Narnaware",
+    description: "Premium Digital products that helps to complete your need ",
+    keywords: [
+      "Products",
+      "DigitalProducts",
+      "WebTemplates",
+      "WebSites",
+      "Ebooks",
+      "Templates",
+      "Presets",
+    ],
+    images: ["/gaurav.svg"],
+    url: "/Products",
+  },
+  openGraph: {
+    title: "Products || Gaurav Narnaware",
+    description: "Premium Digital products that helps to complete your need ",
+    keywords: [
+      "Products",
+      "DigitalProducts",
+      "WebTemplates",
+      "WebSites",
+      "Ebooks",
+      "Templates",
+      "Presets",
+    ],
+    images: ["/gaurav.svg"],
+    url: "/Products",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
+
+
 const ProductLayout = ({ children }) => {
   const HeaderName = (props) => {
     return (
--- src/app/Products/page.js
+++ src/app/Products/page.js
@@ -2,66 +2,7 @@
 import Allproducts from "@/Components/Product/Allproducts";
 import React from "react";
 import { useState } from "react";
-export const metadata = {
-  title: "Products || Gaurav Narnaware",
-  description: "Premium Digital products that helps to complete your need ",
-  keywords: [
-    "Products",
-    "DigitalProducts",
-    "WebTemplates",
-    "WebSites",
-    "Ebooks",
-    "Templates",
-    "Presets",
-  ],
-  images: ["/gaurav.svg"],
-  url: "/Products",
-  twitter: {
-    card: "summary_large_image",
-    title: "Products || Gaurav Narnaware",
-    description: "Premium Digital products that helps to complete your need ",
-    keywords: [
-      "Products",
-      "DigitalProducts",
-      "WebTemplates",
-      "WebSites",
-      "Ebooks",
-      "Templates",
-      "Presets",
-    ],
-    images: ["/gaurav.svg"],
-    url: "/Products",
-  },
-  openGraph: {
-    title: "Products || Gaurav Narnaware",
-    description: "Premium Digital products that helps to complete your need ",
-    keywords: [
-      "Products",
-      "DigitalProducts",
-      "WebTemplates",
-      "WebSites",
-      "Ebooks",
-      "Templates",
-      "Presets",
-    ],
-    images: ["/gaurav.svg"],
-    url: "/Products",
-  },
 
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-};
 
 const AllProducts = () => {
   const [page, setpage] = useState(1);
